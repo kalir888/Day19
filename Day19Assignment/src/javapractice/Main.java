14789fb2b6a45012f7cc973c21a43d9602f3da5f
Kalir888 <kalir888@gmail.com>
2022-04-18 06:24:41 +0530

    First Name Added

--- Day19Assignment/src/javapractice/Main.java
+++ Day19Assignment/src/javapractice/Main.java
@@ -1,8 +1,19 @@
 package javapractice;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner get = new Scanner(System.in);
+        System.out.println("Welcome to User Registration");
+        System.out.println("Enter Your First Name: ");
+        String firstName = get.nextLine();
+        Pattern pattern = Pattern.compile("^([A-Z]{1})([a-z]{2})([a-z]*)$");
+        Matcher matchName1 = pattern.matcher(firstName);
+        if(!matchName1.find())
+            System.out.println("Invalid First Name");
     }
 }
